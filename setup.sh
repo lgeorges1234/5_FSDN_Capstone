b7781a5dfe25c79d2b386534c3d0f29b8532c24f
Your Name <you@example.com>
2023-09-15 10:07:29 +0200

    setup.sh corrected

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export AUTH0_DOMAIN = 'dev-h6cc78r73kusntyh.us.auth0.com'
-export ALGORITHMS = ['RS256']
-export API_AUDIENCE = 'airlines'
+export AUTH0_DOMAIN='dev-h6cc78r73kusntyh.us.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='airlines'
